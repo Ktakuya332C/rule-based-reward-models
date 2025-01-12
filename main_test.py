6893ae8149ae5f425a5c1f707a2f5bf988768dc2
Takuya Kato <17094694+Ktakuya332C@users.noreply.github.com>
2025-01-12 17:22:14 +0900

    Remove {tmp_path}

--- main_test.py
+++ main_test.py
@@ -3,7 +3,7 @@ import main
 def test_end2end(tmp_path):
     main.main([
         "dummy.py",
-        "--save-dir={tmp_path}",
+        f"--save-dir={tmp_path}",
         "--tokenizer-path=openai-community/gpt2",
         "--dataset-type=gsm8k",
     ])
